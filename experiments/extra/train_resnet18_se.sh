b1c2d14b3168d0c591a5122081148cea4d48ffd6
Frank Lu <lu.phrank@gmail.com>
2025-04-09 20:56:14 +0100

    lets try this out.

---
+++ experiments/extra/train_resnet18_se.sh
@@ -0,0 +1,15 @@
+echo "==== Running ResNet18_se_fc512 with Default Settings ===="
+python main.py \
+-s veri \
+-t veri \
+-a resnet18_se \
+--root src/datasets/ \
+--height 224 \
+--width 224 \
+--optim amsgrad \
+--lr 0.0003 \
+--max-epoch 10 \
+--stepsize 20 40 \
+--train-batch-size 64 \
+--test-batch-size 100 \
+--save-dir logs/resnet18_se-veri
