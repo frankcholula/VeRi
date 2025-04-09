e0f8f87fe320b741ca5343c0ec10029a27dbb5c5
Frank Lu <lu.phrank@gmail.com>
2025-04-09 21:21:59 +0100

    added resnet50 se without fc512.

---
+++ experiments/extra/train_resnet50_se.sh
@@ -0,0 +1,15 @@
+echo "==== Running ResNet50_se_fc512 with Default Settings ===="
+python main.py \
+-s veri \
+-t veri \
+-a resnet50_se_fc512 \
+--root src/datasets/ \
+--height 224 \
+--width 224 \
+--optim amsgrad \
+--lr 0.0003 \
+--max-epoch 10 \
+--stepsize 20 40 \
+--train-batch-size 64 \
+--test-batch-size 100 \
+--save-dir logs/resnet50_se_fc512-veri
