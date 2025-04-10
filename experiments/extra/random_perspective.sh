85b7ec9d414963633d484d1ce99d937acf960532
frankcholula <lu.phrank@gmail.com>
2025-04-10 09:58:17 +0100

    added a script for random perspective.

---
+++ experiments/extra/random_perspective.sh
@@ -0,0 +1,16 @@
+echo "==== Trying Random Perspective with Default Settings ===="
+python main.py \
+-s veri \
+-t veri \
+-a resnet18_fc512 \
+--root src/datasets/ \
+--height 224 \
+--width 224 \
+--optim amsgrad \
+--lr 0.0003 \
+--max-epoch 10 \
+--stepsize 20 40 \
+--train-batch-size 64 \
+--test-batch-size 100 \
+--random-perspective \
+--save-dir logs/resnet18_se_fc512-veri
